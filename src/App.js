63aea6f27647e8812ead569f38d0ec12f99d53e7
Aditya <aditya@gravityilabs.com>
2021-01-20 22:57:40 +0530

    web store implementation of React Context pending ;
    Signed-off-by: Aditya <aditya@gravityilabs.com>

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
+import React from "react";
+import Navbar from "./containers/Navbar/Navbar";
 import "./App.css";
+import DataProvider from "./components/DataWebStore/DataProvider";
 
-function App() {
-	return <div className="App">Hello Aditya !!</div>;
+class App extends React.Component {
+	render() {
+		return (
+			<DataProvider>
+				<div className="App">
+					<Navbar />
+				</div>
+			</DataProvider>
+		);
+	}
 }
 
 export default App;
---
+++ src/containers/Navbar/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import DataProvider from "../../components/DataWebStore/DataProvider";
+
+class Navbar extends React.Component {
+    render() {
+        return (
+            // TODO
+
+        );
+    }
+}
+export default Navbar;
