0e472bdc496b4f84937de4ebe4c97454d9a97e73
Aditya Kumar <adikid1996@gmail.com>
2021-05-15 19:42:58 +0530

    setup #1

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import { profile } from './manifest';
+
+function App() {
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/assets/images.js
@@ -0,0 +1,11 @@
+const images = {
+    navigation: {
+        socialHeaders: {
+            twitterIcon: require('./img/twitter.svg'),
+            githubIcon: require('./img/github.svg'),
+            linkedinIcon: require('./img/linkedin.svg')
+        }
+    }
+}
+
+export default images;
